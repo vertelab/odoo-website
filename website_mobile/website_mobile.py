b40fca253dfbe949ecd93aa28fc900e8d63455ef
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-10-04 18:34:00 +0200

    website_mobile: mobile_crud

--- website_mobile/website_mobile.py
+++ website_mobile/website_mobile.py
@@ -28,4 +28,55 @@ import werkzeug
 import logging
 _logger = logging.getLogger(__name__)
 
+BASE = 'Kalle'
 
+class mobile_crud(http.Controller):
+    
+    def __init__(self):
+        self.model = 'res.partner'
+        self.search_domain = [('type','=','contact')]
+        self.fields =  ['name','is_company','phone','email','type']
+        self.fields_info = {'is_company': {'type': 'hidden','default': 'true'}}
+        self.template = {'list': '%s.object_list' % __name__, 'detail': '%.object_detail' % __name__}
+        self.limit = 25
+        self.order = 'name'
+        self.notifications = []  # list of dicts message, type, action_one, action_two 
+
+
+    def do_action(self,action=['edit','add','delete','search'],obj=None,search=None,**post,request):
+        
+        if request.httprequest.url[-4:] == 'edit' and 'edit' in action: #Edit
+            if request.httprequest.method == 'GET':
+                return request.render(self.template['detail'], {'model': self.model, 'object': obj, 'fields': self.fields, 'root': MODULE_BASE_PATH, 'title': obj.name, 'db': request.db, 'mode': 'edit'})
+            else:
+                obj.write({
+                    f: post.get(f) for f in self.fields
+                })
+                return request.render(self.template['detail'], {'model': self.model, 'object': obj, 'fields': self.fields, 'root': MODULE_BASE_PATH, 'title': obj.name, 'db': request.db, 'mode': 'view'})
+        elif request.httprequest.url[-3:] == 'add' and 'add' in action: #Add
+            if request.httprequest.method == 'GET':
+                return request.render(self.template['detail'], {'model': self.model, 'object': None, 'fields': self.fields, 'root': MODULE_BASE_PATH, 'title': 'Add User', 'db': request.db,'mode': 'add'})
+            else:
+                record = { f: post.get(f) for f in self.fields }
+                partner = request.env[self.model].create(record)
+                return request.render(self.template['detail'], {'model': self.model, 'object': obj, 'fields': self.fields, 'root': MODULE_BASE_PATH, 'title': obj.name, 'db': request.db, 'mode': 'view'})
+        elif request.httprequest.url[-6:] == 'delete' and 'delete' in action: #Delete
+            if obj:
+                obj.unlink()
+                return werkzeug.utils.redirect(MODULE_BASE_PATH, 302)
+        elif request.httprequest.url[-6:] == 'search': #Search
+            if request.httprequest.method == 'POST':
+            self.search_domain.append(('name','ilike',post.get('search_words')))
+        elif obj:  # Detail
+            return request.render(self.template['detail'], {'model': self.model, 'object': obj, 'fields': self.fields, 'root': MODULE_BASE_PATH, 'title': obj.name, 'db': request.db, 'mode': 'view'})
+        return request.render(self.template['list'], {
+            'objects': self.search(search=post.get('search_word'),request),
+            'title': MODULE_TITLE,
+            'root': MODULE_BASE_PATH,
+            'db': request.db,
+        })
+
+    def search(self,search=None,request):
+        if search:
+            self.search_domain.append(('name','ilike',search))
+        return request.env[self.model].search(self.search_domain, order=self.order, limit=self.limit)
