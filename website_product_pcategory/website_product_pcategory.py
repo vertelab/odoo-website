6c75efcf73309a88244524f8e6862c76acca3305
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-11-24 17:43:16 +0100

    website_product_pcategory: new table

--- website_product_pcategory/website_product_pcategory.py
+++ website_product_pcategory/website_product_pcategory.py
@@ -40,9 +40,9 @@ class website_product_category(http.Controller):
         if category and not category.website_published:
             return request.render('website.page_404', {})
         elif category:
-            return request.render('website_product_pcategory.landing_page', {'products': request.env['product.template'].sudo().search(['&', (category.id, 'in', 'public_categ_ids'), ('state', '=', 'sellable')]), 'category': category})
+            return request.render('website_product_pcategory.landing_page', {'products': request.env['product.template'].sudo().search(['&', ('public_categ_ids', 'in',category.id ), ('state', '=', 'sellable'),('website_published', '=', True)]), 'category': category})
         else:
-            return request.render('website_product_pcategory.list_category', {})
+            return request.render('website_product_pcategory.category_listing', {})
 
     @http.route(['/pcategory/<model("product.public.category"):category>/products', ], type='http', auth="public", website=True)
     def get_category_products(self, category=False,**post):
@@ -50,14 +50,20 @@ class website_product_category(http.Controller):
         if category and not category.website_published:
             return request.render('website.page_404', {})
         else:
-            return request.render('website_product_pcategory.product_view', {'products': request.env['product.template'].sudo().search(['&', (category.id, 'in', 'public_categ_ids'), ('state', '=', 'sellable')]), 'category': category})
+            return request.render('website_product_pcategory.product_listing', {'product_ids': request.env['product.template'].sudo().search(['&',  ('public_categ_ids', 'in',category.id ), ('state', '=', 'sellable'),('website_published', '=', True)]), 'category': category})
 
 
 class product_public_category(models.Model):
     _inherit = "product.public.category"
     
-    website_description = fields.Html('Description for the website', translate=True)
+    website_short_desc = fields.Text('Short Description', help="Short Description for the website to be used in listings", translate=True)
+    website_description = fields.Html('Description for the website medium large', help="Description for the website medium large",translate=True)
     landing_page = fields.Html('Landing page for website', translate=True)
     website_published = fields.Boolean('Available in the website', copy=False)
+    # Taken from OCA website_sale_product_category_seo
+    cms_top = fields.Html(help="You can insert a html snippet here it will be"
+                               "displayed on top of this categorie")
+    cms_bottom = fields.Html(help="You can insert a html snippet here it will"
+                                  "be displayed on bottom of this categorie")
 
 
