ad980e7f803df13e665ff52bad11a04ab79707d2
Haojun Zou <apollo_zhj@msn.com>
2017-04-04 16:38:40 +0200

    website_fts: history search

--- website_fts/static/src/js/main.js
+++ website_fts/static/src/js/main.js
@@ -3,13 +3,12 @@ $(document).ready(function(){
         openerp.jsonRpc("/search_response", "call", {
             'search': $(this).val(),
         }).done(function(data){
-            console.log(data);
             content_front = '<div class="result_suggestion">';
             content_behind = '</div>';
             content = '';
             $.each(data, function(key, info) {
                 if (data[key]['model_record'] == 'product.template'){
-                    c = '<li style="list-style-type: none;"><a style="color: #333; padding: 3px 20px; clear: both; font-weight: normal;" href="/shop/product/' + data[key]['res_id'] + '">' + data[key]['name'] + '</a></li>';
+                    c = '<li style="list-style-type: none;"><a href="/shop/product/' + data[key]['res_id'] + '">' + data[key]['name'] + '</a></li>';
                     content += c;
                 }
             });
