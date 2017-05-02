b3bf59793918ef0e5b23ab93a145642a87439ee4
Robin Chatfield <robin.chatfield@vertel.se>
2017-05-02 15:46:45 +0200

    website_menu_groups: Module created.

--- portal_remove_mail_footer/mail.py
+++ portal_remove_mail_footer/mail.py
@@ -39,3 +39,46 @@ class MailMail(models.Model):
         else:
             return None
 
+#~ class MailNotification(models.Model):
+    #~ _name = 'mail.notification'
+
+    #~ def get_signature_footer(self, cr, uid, user_id, res_model=None, res_id=None, context=None, user_signature=True):
+        #~ """ Format a standard footer for notification emails (such as pushed messages
+            #~ notification or invite emails).
+            #~ Format:
+                #~ <p>--<br />
+                    #~ Administrator
+                #~ </p>
+                #~ <div>
+                    #~ <small>Sent from <a ...>Your Company</a> using <a ...>OpenERP</a>.</small>
+                #~ </div>
+        #~ """
+        #~ footer = ""
+        #~ if not user_id:
+            #~ return footer
+
+        #~ # add user signature
+        #~ user = self.pool.get("res.users").browse(cr, SUPERUSER_ID, [user_id], context=context)[0]
+        #~ if user_signature:
+            #~ if user.signature:
+                #~ signature = user.signature
+            #~ else:
+                #~ signature = "--<br />%s" % user.name
+            #~ footer = tools.append_content_to_html(footer, signature, plaintext=False)
+
+        #~ # add company signature
+        #~ if user.company_id.website:
+            #~ website_url = ('http://%s' % user.company_id.website) if not user.company_id.website.lower().startswith(('http:', 'https:')) \
+                #~ else user.company_id.website
+            #~ company = "<a style='color:inherit' href='%s'>%s</a>" % (website_url, user.company_id.name)
+        #~ else:
+            #~ company = user.company_id.name
+        #~ sent_by = _('Sent by %(company)s using %(odoo)s')
+
+        #~ signature_company = '<br /><small>%s</small>' % (sent_by % {
+            #~ 'company': company,
+            #~ 'odoo': "<a style='color:inherit' href='https://www.odoo.com/'>Odoo</a>"
+        #~ })
+        #~ footer = tools.append_content_to_html(footer, signature_company, plaintext=False, container_tag='div')
+
+        #~ return footer
