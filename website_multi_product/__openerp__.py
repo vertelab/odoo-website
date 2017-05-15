530422ca27b2983eb5d71c18bfc3c21f21e11574
Anders Wallenquist <anders.wallenquist@vertel.se>
2017-05-15 17:36:34 +0200

    website_page_groups: new module

---
+++ website_multi_product/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Multi Website Product domain',
+    'category': 'Website',
+    'summary': 'Build Multiple Websites and add a product domain',
+    'website': 'http://www.vertel.se',
+    'version': '1.0',
+    'description': """
+Multi Website Product domain
+============================
+
+        """,
+    'author': 'Vertel AB',
+    'depends': ['website_multi','website_sale'],
+    'installable': True,
+    'data': [
+        'res_config.xml',
+        'website_views.xml',
+    ],
+    'demo' : [
+    ],
+    'application': True,
+}
