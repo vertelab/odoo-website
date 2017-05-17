93daa0a8017df4ef3a6f222983e160658712c6f2
Haojun Zou <apollo_zhj@msn.com>
2017-05-17 16:07:53 +0200

    website_masonry: added controller

---
+++ website_masonry/website.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2017- Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+from openerp import http
+from openerp.http import request
+from datetime import datetime
+from lxml import html
+import werkzeug
+
+
+class Masonry(http.Controller):
+
+    @http.route(['/page/demo_masonry'], type='http', auth="user", website=True)
+    def demo_masonry(self):
+        return request.website.render('website_masonry.demo_masonry', {})
