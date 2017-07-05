5e7f887f9b3693fd29efb95dba80bc2764a3d4a7
Haojun Zou <apollo_zhj@msn.com>
2017-07-05 17:50:37 +0200

    website_theme_overlay_menu: changed overflow behavior and disable background scroll when overlay activated

--- website_theme_overlay_menu/static/src/js/main.js
+++ website_theme_overlay_menu/static/src/js/main.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
 
 function openNav() {
     $("#overlay_nav").css({"height": "100%"});
+    $("body").css({"position": "fixed", "overflow": "hidden"});
 }
 
 function closeNav() {
     $("#overlay_nav").css({"height": "0%"});
+    $("body").css({"position": "", "overflow": ""});
 }
