e419dca8e99f7581ee3f75e1b154aeddfeb475d6
Robin Chatfield <robin.chatfield@vertel.se>
2018-05-09 13:14:33 +0200

    website_fts: Removed old implementation.

--- website_fts_attachment/ir_attachment.py
+++ website_fts_attachment/ir_attachment.py
@@ -24,56 +24,12 @@ from openerp import models, fields, api, _
 import logging
 _logger = logging.getLogger(__name__)
 
-
-class fts_fts(models.Model):
-    _inherit = 'fts.fts'
-
-    facet = fields.Selection(selection_add=[('document','Document'),('image','Image')])
-
-    @api.one
-    def get_object(self,words):
-        if self.res_model == 'ir.attachment':
-            page = self.env['ir.attachment'].browse(self.res_id)
-            return {'name': page.name, 'body': self.get_text([self.description,page.index_context],words)}
-        return super(fts_fts, self).get_object()
-
 class document_file(models.Model):
     _name = 'ir.attachment'
     _inherit = ['ir.attachment', 'fts.model']
 
-
+    # TODO: Not needed anymore?
     group_ids = fields.Many2many(string='Groups', comodel_name='res.groups')
-
-    _fts_fields = ['index_content','name','description']
-    #~ _fts_fields_d = [{'name': 'index_content'}, {'name': 'name'}, {'name': 'description'}]
 
     def _get_fts_fields(self):
         return [{'name': 'index_content'}, {'name': 'name'}, {'name': 'description'}]
-    
-    #~ @api.depends('index_content','name','description')
-    #~ @api.one
-    #~ def _compute_fts_trigger(self):
-        #~ """
-        #~ Dummy field to trigger the updates on SQL level. Tracking
-        #~ changes is much easier on Odoo level than on SQL level. Make
-        #~ this field dependant on the relevant fields.
-        #~ """
-        #~ # TODO: Trigger this update when relevant translations change.
-        #~ if self._fts_trigger:
-            #~ self._fts_trigger = True
-        #~ else:
-            #~ self._fts_trigger = False
-
-    @api.one
-    def _full_text_search_update(self):
-        if self.url and (self.url.startswith('/web/js/') or self.url.startswith('/web/css/')):
-            return
-        super(document_file, self)._full_text_search_update()
-        self.env['fts.fts'].update_html(self._name, self.id, html=' '.join([h for h in [self.index_content, self.name, self.description] if h]), groups=self.group_ids, rank=10)
-        if self.file_type and 'document' in self.file_type:
-            self.env['fts.fts'].update_text(self._name,self.id,text=self.name,facet='document',groups=self.group_ids, rank=0)
-        if self.file_type and 'image' in self.file_type:
-            self.env['fts.fts'].update_text(self._name,self.id,text=self.name,facet='image',groups=self.group_ids, rank=0)
-        # Exif metadata ????
-
-
--- website_fts_product/product.py
+++ website_fts_product/product.py
@@ -24,52 +24,15 @@ from openerp import models, fields, api, _
 import logging
 _logger = logging.getLogger(__name__)
 
-class fts_fts(models.Model):
-    _inherit = 'fts.fts'
-
-    facet = fields.Selection(selection_add=[('product_template','Product Template'), ('product_product','Product Product'), ('product_public_category','Product Public Category')])
-
-    @api.one
-    def get_object(self, words):
-        if self.res_model == 'product.template':
-            product = self.env['product.template'].browse(self.res_id)
-            if product:
-                return {'name': product.name, 'body': self.get_text([product.name, product.description_sale], words)}
-        elif self.res_model == 'product.product':
-            product = self.env['product.product'].browse(self.res_id)
-            if product:
-                return {'name': product.name, 'body': self.get_text([product.name, product.description_sale, product.default_code], words)}
-        elif self.res_model == 'product.public.category':
-            category = self.env['product.public.category'].browse(self.res_id)
-            if category:
-                return {'name': category.name, 'body': self.get_text([category.name], words)}
-        return super(fts_fts, self).get_object()
-
-
 class product_template(models.Model):
     _name = 'product.template'
     _inherit = ['product.template', 'fts.model']
-
-    _fts_fields = ['website_published', 'name', 'description_sale']
 
     def _get_fts_fields(self):
         return [
             {'name': 'name', 'weight': 'A'},
             {'name': 'attribute_line_ids.value_ids.name', 'weight': 'A'},
             {'name': 'description_sale', 'weight': 'B'}]
-
-    @api.one
-    def _full_text_search_update(self):
-        # TODO: Remove the context as it doesn't belong in a general purpose module.
-        # This is starting to be a too comon problem. Probably should change the checks to only activate in certain views.
-        super(product_template, self).with_context(suppress_checks=True)._full_text_search_update()
-        self._full_text_search_delete()
-        self.write({'fts_dirty': False})
-        if self.website_published and self.active:
-            self.env['fts.fts'].update_text(self._name, self.id, text=self.name, rank=0)
-            if self.description_sale:
-                self.env['fts.fts'].update_text(self._name, self.id, text=self.description_sale, rank=5)
-            #~ self.env['fts.fts'].update_text(self._name,self.id,text=self.author_id.name,facet='author',rank=int(self.ranking))
 
     @api.model
     def fts_get_default_suggestion_domain(self):
@@ -82,8 +45,6 @@ class product_product(models.Model):
     _name = 'product.product'
     _inherit = ['product.product', 'fts.model']
 
-    _fts_fields = ['website_published','name','description_sale','default_code','ean13','product_tmpl_id','attribute_value_ids']
-    
     def _get_fts_fields(self):
         return [
             {'name': 'name', 'weight': 'A', 'related': 'product_tmpl_id.name', 'related_table': 'product_template'},
@@ -92,16 +53,6 @@ class product_product(models.Model):
             {'name': 'default_code', 'weight': 'A'},
             {'name': 'ean13', 'weight': 'A'}]
     _fts_trigger = fields.Boolean(string='Trigger FTS Update', help='Change this field to update FTS.', compute='_compute_fts_trigger', store=True)
-
-    @api.one
-    def _full_text_search_update(self):
-        # TODO: Remove the context as it doesn't belong in a general purpose module.
-        super(product_product, self).with_context(suppress_checks=True)._full_text_search_update()
-        self._full_text_search_delete()
-        self.write({'fts_dirty': False})
-        if self.website_published and self.active:
-            self.env['fts.fts'].update_text(self._name, self.id, text=' '.join([self.name, self.default_code, self.ean13]), rank=0)
-            self.env['fts.fts'].update_text(self._name, self.id, text=(self.description_sale or '')+' '+ ' '.join([att.name for att in self.attribute_value_ids]), rank=5)
 
     @api.model
     def fts_get_default_suggestion_domain(self):
@@ -126,13 +77,5 @@ class product_public_category(models.Model):
     _name = 'product.public.category'
     _inherit = ['product.public.category', 'fts.model']
 
-    _fts_fields = ['name']
-    #~ _fts_fields_d = [{'name': 'name', 'weight': 'A'}]
-
     def _get_fts_fields(self):
         return [{'name': 'name', 'weight': 'A'}]
-
-    @api.one
-    def _full_text_search_update(self):
-        super(product_public_category, self)._full_text_search_update()
-        self.env['fts.fts'].update_text(self._name, self.id, text=self.name, rank=5)
