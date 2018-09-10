04c5191cbf371347d48129bb9ac5073ec6815965
Haojun Zou <apollo_zhj@msn.com>
2018-09-10 11:20:26 +0200

    website_theme_demo_page: new module, need more work on color picker table

--- website_theme_overlay_menu/__manifest__.py
+++ website_theme_overlay_menu/__manifest__.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution, third party addon
-#    Copyright (C) 2017- Vertel AB (<http://vertel.se>).
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2017- Vertel (<http://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
