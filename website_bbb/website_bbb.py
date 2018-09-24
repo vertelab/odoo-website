5ff887d4ceb514c4e2dc3724c940870e171f96e6
Robin Chatfield <robin.chatfield@vertel.se>
2018-09-24 12:17:02 +0200

    website_bbb_event: Module created.

--- website_bbb/website_bbb.py
+++ website_bbb/website_bbb.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 from openerp import models, fields, api, _
+from openerp.exceptions import Warning
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -27,7 +28,9 @@ import urllib
 import urllib2
 import sha
 import uuid
-import xml.etree.ElementTree
+from xml.etree import ElementTree
+import traceback
+import sys
 
 class BBBServer(models.Model):
     _name = 'bbb.server'
@@ -41,14 +44,13 @@ class BBBServer(models.Model):
 class BBBMeeting(models.Model):
     _name = 'bbb.meeting'
     _description = 'Big Blue Button Meeting'
-    
-    
+
     def _default_bbbid(self):
         return uuid.uuid4()
-    
+
     def _default_server_id(self):
         return self.env['bbb.server'].search([], limit=1)
-    
+
     name = fields.Char(string='Name', required=True)
     welcome = fields.Char(string='Welcome Message', required=True)
     bbbid = fields.Char(string='BBB ID', default=_default_bbbid, required=True)
@@ -68,18 +70,27 @@ class BBBMeeting(models.Model):
         return '%s/bigbluebutton/api/%s?%s' % (self.server_id.url, func, param_str)
 
     @api.multi
-    def action_start_meeting(self):
+    def start_meeting(self):
         url = self.get_url('create', name=self.name, meetingID=self.bbbid, attendeePW=self.password, moderatorPW=self.mod_password, welcome=self.welcome)
+        res = ""
         try:
-            res = urllib2.urlopen(url)
-            return ElementTree.fromstring(res.read()).find('returncode').text == 'SUCCESS'
+            res = urllib2.urlopen(url).read()
+            return ElementTree.fromstring(res).find('returncode').text == 'SUCCESS'
         except:
+            tb = ''.join(traceback.format_exception(*sys.exc_info()))
+            _logger.warn("Failed to start Big Blue Button Meeting\n\nReply:%s\n%s" % (res, tb))
             return False
+
+    @api.multi
+    def action_start_meeting(self):
+        if self.start_meeting():
+            raise Warning(_("Meeting started."))
+        raise Warning(_("Failed to start meeting!"))
 
     @api.multi
     def invite(self, name, moderator=False):
         return self.get_url('join', fullName=name, meetingID=self.bbbid, password=moderator and self.mod_password or self.password)
-    
+
     @api.multi
     def action_join(self):
         wizard = self.env['bbb.join.wizard'].create({
@@ -89,28 +100,18 @@ class BBBMeeting(models.Model):
             'name': _('Join Meeting'),
             'view_type': 'form',
             'view_mode': 'form',
-            # ~ 'view_id': [res_id],
             'res_model': 'bbb.join.wizard',
             'context': "{}",
             'type': 'ir.actions.act_window',
-            # ~ 'nodestroy': True,
             'target': 'new',
             'res_id': wizard.id,
         }
-    
+
     @api.multi
-    def invite_partners(self, template, partners):
+    def invite_partners(self, template, partners, moderator=False):
         for partner in partners:
-            join_url = self.invite(partner.name)
+            join_url = self.invite(partner.name, moderator)
             template.with_context(join_url=join_url).send_mail(partner.id, force_send=True, raise_exception=True)
-    
-    # ~ get_url('create', name=self.name, meetingID=self.bbbid, attendeePW='luser', moderatorPW='pcmasterrace', welcome="Tjaba tjena hallå.")
-
-    # ~ get_url('join', fullName='Bob Ross', meetingID='soulsuck', password='pcmasterrace')
-    # ~ get_url('join', fullName='Billy-Bob Thornton', meetingID='soulsuck', password='luser')
-
-
-    # ~ auth_signup_2wexpire,webshop_memcached_dermanord,website_sale_price_chart,website_fts,theme_dermanord,webshop_dermanord,website_sale_home,website_reseller_register,website_theme_demo_page
 
 class BBBJoinWizard(models.TransientModel):
     _name = 'bbb.join.wizard'
