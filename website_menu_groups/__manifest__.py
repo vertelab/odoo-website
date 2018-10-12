c745193ccc28821ec12c40783d145a72a6a4f260
Haojun Zou <apollo_zhj@msn.com>
2018-10-12 15:24:17 +0200

    website_menu_groups: changed tag on xml file

--- website_menu_groups/__manifest__.py
+++ website_menu_groups/__manifest__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2017 Vertel AB (<http://vertel.se>).
+#    Copyright (C) 2014-2017 Vertel AB <http://vertel.se>
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -28,8 +27,8 @@
     'website': 'http://www.vertel.se',
     'depends': ['website'],
     'data': [
-        'website_view.xml',
         'security/website_security.xml',
+        'website_view.xml',
         ],
     'application': False,
     'installable': True,
