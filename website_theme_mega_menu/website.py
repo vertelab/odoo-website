53ca0aa47f92def6137cd2decad9ffe90a98b221
sandra pettersson <sandra.pettersson@vertel.se>
2019-11-21 08:40:03 +0100

    website_theme_mega_menu: fixed styling and functions for mega menu

--- website_theme_mega_menu/website.py
+++ website_theme_mega_menu/website.py
@@ -28,13 +28,18 @@ class website(models.Model):
     _inherit = "website"
     
     def get_mega_menu_categories(self):
-        return [
-            self.env.ref('__export__.product_public_category_4'),
-            self.env.ref('__export__.product_public_category_3'),
-            self.env.ref('__export__.product_public_category_2'),
-            self.env.ref('__export__.product_public_category_5'),
-            self.env.ref('__export__.product_public_category_6'),
-        ]
+        children = self.env['product.public.category'].search([('parent_id', '=', None), ('website_published', '=', True)], order='sequence asc')
+        _logger.warn('sandra %s' % children.mapped('name'))
+
+        return children 
+        
+            # self.env.ref('__export__.product_public_category_4'),
+            # self.env.ref('__export__.product_public_category_3'),
+            # self.env.ref('__export__.product_public_category_2'),
+            # self.env.ref('__export__.product_public_category_5'),
+            # self.env.ref('__export__.product_public_category_6'),
+
+
         
         
     def make_categ_link(self, value):
