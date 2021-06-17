d48c1f7357294a924a10430a3ad310825e176658
Miracle Ayodele <miracleaayodele@gmail.com>
2021-06-17 14:50:25 +0100

    website_slides_copy

---
+++ website_slides_copy/__manifest__.py
@@ -0,0 +1,19 @@
+# Part of Softhealer Technologies.
+{
+    "name": "Website Slide Copy",
+    "author": "Vertel",
+    "website": "https://www.vertel.se",
+    "category": "Website",
+    "license": "OPL-1",
+    "summary": "Wizard to copy slide section with slides into selected slide.channel object.",
+    "description": """"Wizard to copy slide section with slides into selected slide.channel object.""",
+    "version": "14.0.1.0",
+    "depends": ['website_slides'],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/slide_channel_wizard.xml',
+    ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
