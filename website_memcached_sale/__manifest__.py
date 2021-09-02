0021d11d5f5c2639ef4b5aa2eddda25c82432392
Simon Rundstedt <simon.rundstedt@vertel.se>
2021-09-02 09:33:37 +0000

    Tentative removal of product_private dependency.

--- website_memcached_sale/__manifest__.py
+++ website_memcached_sale/__manifest__.py
@@ -32,7 +32,7 @@ This module depends on website_memcached
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'website': 'http://www.vertel.se',
-    'depends': ['website_memcached','website_sale', 'product_private'],
+    'depends': ['website_memcached','website_sale'],
     'external_dependencies': {'python': ['pymemcache',]},
     'data': [
         'website_view.xml',
