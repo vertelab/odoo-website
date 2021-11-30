78dce4194667a17856ea88a421074def048e004a
iammiracle <miracleaayodele@gmail.com>
2021-11-30 16:58:59 +0100

    list my events on website

---
+++ website_event_portal_listing/__manifest__.py
@@ -0,0 +1,18 @@
+# Part of Softhealer Technologies.
+{
+    "name": "Website Event Portal Listing",
+    "author": "Vertel",
+    "website": "https://www.vertel.se",
+    "category": "Website",
+    "license": "OPL-1",
+    "summary": "User can see events registered for.",
+    "description": """"User can see events registered for.""",
+    "version": "14.0.1.0",
+    "depends": ['website_event'],
+    "data": [
+        'views/event_portal_templates.xml',
+    ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
