e94b4010c13e6a94330bd7b8b5deb6d955017a72
iammiracle <miracleaayodele@gmail.com>
2022-01-21 17:29:23 +0100

    website document activities dev

--- website_document_activities/__manifest__.py
+++ website_document_activities/__manifest__.py
@@ -10,12 +10,12 @@
         User can see activities from the portal \n	
     """,
     "version": "14.0.1.1",
-    "depends": ['portal', 'sale', 'project', 'dms'],
+    "depends": ['portal', 'sale', 'project'],
     "data": [
         'views/portal_template.xml',
         'views/sale_portal_templates.xml',
         'views/project_portal_templates.xml',
-        'views/document_portal_templates.xml',
+        # 'views/document_portal_templates.xml',
         'views/assets.xml',
     ],
     "auto_install": False,
