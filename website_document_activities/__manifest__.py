8b9c5f1d923a1dac2f5ca7023c8c038fb0943095
iammiracle <miracleaayodele@gmail.com>
2022-01-21 17:04:43 +0100

    website document activities dev

---
+++ website_document_activities/__manifest__.py
@@ -0,0 +1,23 @@
+# Part of Softhealer Technologies.
+{
+    "name": "Website Document Activities",
+    "author": "Vertel",
+    "website": "https://www.vertel.se",
+    "category": "Website",
+    "license": "OPL-1",
+    "summary": "User can see events registered for.",
+    "description": """"
+        User can see activities from the portal \n	
+    """,
+    "version": "14.0.1.1",
+    "depends": ['portal', 'sale', 'project'],
+    "data": [
+        'views/portal_template.xml',
+        'views/sale_portal_templates.xml',
+        'views/project_portal_templates.xml',
+        'views/assets.xml',
+    ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
---
+++ website_document_activities/static/src/js/portal.js
@@ -0,0 +1,59 @@
+odoo.define('website_document_activities.portal', function (require) {
+    'use strict';
+
+    var publicWidget = require('web.public.widget');
+    const Dialog = require('web.Dialog');
+    const {_t, qweb} = require('web.core');
+    const ajax = require('web.ajax');
+
+    publicWidget.registry.PortalHomeCounters = publicWidget.Widget.extend({
+        selector: '.o_portal_my_home',
+
+        start: function () {
+            var def = this._super.apply(this, arguments);
+            this._updateActivitiesCounters();
+            this._updateCounters();
+            return def;
+        },
+
+        async _updateActivitiesCounters(elem) {
+            const numberRpc = 3;
+            const needed = this.$('[data-placeholder_activities_count]')
+                                    .map((i, o) => $(o).data('placeholder_activities_count'))
+                                    .toArray();
+            const counterByRpc = Math.ceil(needed.length / numberRpc);  // max counter, last can be less
+
+            const proms = [...Array(Math.min(numberRpc, needed.length)).keys()].map(async i => {
+                await this._rpc({
+                    route: "/activities/counters",
+                    params: {
+                        counters: needed.slice(i * counterByRpc, (i + 1) * counterByRpc)
+                    },
+                }).then(data => {
+                    Object.keys(data).map(k => this.$("[data-placeholder_activities_count='" + k + "']").text(data[k]));
+                });
+            });
+            return Promise.all(proms);
+        },
+
+        async _updateCounters(elem) {
+            const numberRpc = 3;
+            const needed = this.$('[data-placeholder_count]')
+                                    .map((i, o) => $(o).data('placeholder_count'))
+                                    .toArray();
+            const counterByRpc = Math.ceil(needed.length / numberRpc);  // max counter, last can be less
+
+            const proms = [...Array(Math.min(numberRpc, needed.length)).keys()].map(async i => {
+                await this._rpc({
+                    route: "/my/counters",
+                    params: {
+                        counters: needed.slice(i * counterByRpc, (i + 1) * counterByRpc)
+                    },
+                }).then(data => {
+                    Object.keys(data).map(k => this.$("[data-placeholder_count='" + k + "']").text(data[k]));
+                });
+            });
+            return Promise.all(proms);
+        },
+    })
+})
