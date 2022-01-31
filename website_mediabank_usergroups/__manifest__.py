75b77b167e4e452e37f4522beeff7bcb4e4ef6bc
iammiracle <miracleaayodele@gmail.com>
2022-01-31 10:37:07 +0100

    website mediabank usergroups

---
+++ website_mediabank_usergroups/__manifest__.py
@@ -0,0 +1,23 @@
+# Part of Softhealer Technologies.
+{
+    "name": "Website MediaBank User Groups",
+    "author": "Vertel",
+    "website": "https://www.vertel.se",
+    "category": "Website",
+    "license": "OPL-1",
+    "summary": "Only the right group will be able to use the website media",
+    "description": """
+        User with the right group will use the website media
+    """,
+    "version": "14.0.1.2",
+    "depends": ['website', 'web_editor'],
+    "data": [
+        'views/assets.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
