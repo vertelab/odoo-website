6ecce4a76bdd883cd14764ad2dff33c7531fab5d
iammiracle <miracleaayodele@gmail.com>
2022-03-30 12:15:54 +0100

    website_page_menu

---
+++ website_page_menu_form_view/__manifest__.py
@@ -0,0 +1,18 @@
+# Part of Softhealer Technologies.
+{
+    "name": "Website Menu Form View",
+    "author": "Vertel",
+    "website": "https://www.vertel.se",
+    "category": "Website",
+    "license": "OPL-1",
+    "summary": "View website menu form view",
+    "description": """"View website menu form view""",
+    "version": "14.0.1.0",
+    "depends": ['website'],
+    "data": [
+        'views/website_views.xml',
+    ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+}
