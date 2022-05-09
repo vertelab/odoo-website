9b34cac953d6b9f966200cbee98c5fbcab6b3fa3
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-05-09 11:45:09 +0200

    Removed debug logs

--- website_portal_access/controllers/portal.py
+++ website_portal_access/controllers/portal.py
@@ -14,15 +14,11 @@ class CustomerPortal(CustomerPortal):
         """
         Intended to stop a user with knowledge of the id from accessing a sale order in 'draft' state.
         """
-        _logger.warning("Entering customer portal _document_check_access")
         document_sudo = super(CustomerPortal, self)._document_check_access(model_name, document_id, access_token)
         if model_name == "sale.order":
-            _logger.warning("Is a sale order")
             if access_token and document_sudo.access_token and consteq(document_sudo.access_token, access_token):
-                _logger.warning("Has access token")
                 # Has access due to access token
                 return document_sudo
             elif document_sudo.state in ["draft"]:
-                _logger.warning("is in draft")
                 raise AccessError(_("Access Error, state in draft"))
         return document_sudo
