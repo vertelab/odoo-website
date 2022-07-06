5047142f751ad3a42d735575a31f70a91447bb1f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-07-06 11:15:20 +0200

    Update __manifest__.py

--- website_document_activities/__manifest__.py
+++ website_document_activities/__manifest__.py
@@ -1,17 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # Part of Softhealer Technologies.
+
 {
-    "name": "Website Document Activities",
-    "author": "Vertel AB",
-    "website": "https://www.vertel.se",
-    "category": "Website",
-    "license": "OPL-1",
-    "summary": "User can see events registered for.",
+    'name': 'Website: Website Document Activities',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'User can see events registered for.',
+    'category': 'Productivity',
     "description": """
         User can see activities from the portal \n
         14.0.1.3 - Removed 'Actions' from this module and moved to new module
         14.0.1.2 - Link to the activities document
     """,
-    "version": "14.0.1.3",
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-website/website_bootstrap_select2',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-website',
     "depends": ['portal', 'sale', 'project', 'toggle_record_on_portal', 'record_keeping_project', 'web_editor'],
     "data": [
         'views/portal_template.xml',
@@ -23,3 +50,4 @@
     "application": True,
     "installable": True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
