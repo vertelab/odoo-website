1b4f9fd94e5550c1c46665be47ee7e4e6b9d690d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-07-06 11:48:57 +0200

    Update __manifest__.py

--- website_mediabank_usergroups/__manifest__.py
+++ website_mediabank_usergroups/__manifest__.py
@@ -1,15 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # Part of Softhealer Technologies.
+
 {
-    "name": "Website MediaBank User Groups",
-    "author": "Vertel AB",
-    "website": "https://www.vertel.se",
-    "category": "Website",
-    "license": "OPL-1",
+    'name': 'Website: Website MediaBank User Groups',
+    'version': '14.0.1.2',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     "summary": "Only the right group will be able to use the website media",
     "description": """
         User with the right group will use the website media
     """,
-    "version": "14.0.1.2",
+    'description': 'Help with e-services.',
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-website/website_mediabank_usergroups',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-website',
     "depends": ['website', 'web_editor'],
     "data": [
         'views/assets.xml',
@@ -21,3 +48,4 @@
     "application": True,
     "installable": True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
