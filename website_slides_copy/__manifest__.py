a4985b259b454621af687a7d583d89674853bba3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-07-06 13:57:21 +0200

    Update __manifest__.py

--- website_slides_copy/__manifest__.py
+++ website_slides_copy/__manifest__.py
@@ -1,13 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # Part of Softhealer Technologies.
+
 {
-    "name": "Website Slide Copy",
-    "author": "Vertel AB",
-    "website": "https://www.vertel.se",
-    "category": "Website",
-    "license": "OPL-1",
+    'name': 'Website: Website Slide Copy',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     "summary": "Wizard to copy slide section with slides into selected slide.channel object.",
+    'category': 'Website',
     "description": """"Wizard to copy slide section with slides into selected slide.channel object.""",
-    "version": "14.0.1.0",
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-website/website_slide_copy',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+#    "license": "OPL-1",
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-website',
     "depends": ['website_slides'],
     "data": [
         'security/ir.model.access.csv',
@@ -17,3 +45,4 @@
     "application": True,
     "installable": True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
