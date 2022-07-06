2104948b3408223d507d713e87bb6267536e6c13
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-07-06 10:37:34 +0200

    Update __manifest__.py

--- odoo_screen_capture/__manifest__.py
+++ odoo_screen_capture/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) {year} {company} (<{mail}>)
-#    All Rights Reserved
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,26 +15,27 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 #
 # https://www.odoo.com/documentation/14.0/reference/module.html
 #
 {
-    'name': 'Odoo Screen Capture',
-    'version': '14.0.0.0.1',
-    'summary': """
-        Module to allow users to capture website content using javascript.    
-    """,
-    'category': 'Productivity', # Technical Settings|Localization|Payroll Localization|Account Charts|User types|Invoicing|Sales|Human Resources|Operations|Marketing|Manufacturing|Website|Theme|Administration|Appraisals|Sign|Helpdesk|Administration|Extra Rights|Other Extra Rights|
-    'description': """
-        Module to allow users to capture website content using javascript.  
-    """,
-    #'sequence': 1,
+    'name': 'Website: Odoo Screen Capture',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Module to allow users to capture website content using javascript.',
+    'category': 'Productivity',
+    'description': 'Module to allow users to capture website content using javascript.',
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-',
-    'images': ['static/description/banner.png'], # 560x280
+    'website': 'https://vertel.se/apps/odoo-website/odoo_screen_capture',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-website',
     'license': 'AGPL-3',
     'depends': ['base', 'website'],
      #"external_dependencies": {
@@ -51,3 +52,5 @@
     'auto_install': False,
     #"post_init_hook": "post_init_hook",
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
