63566d5c6239af0de03f27a844deccc7913dcba3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-12 13:32:24 +0200

    Update __manifest__.py

--- website_map_input/__manifest__.py
+++ website_map_input/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Get latitude and longitude.',
-    'category': 'Technical',
+    'category': 'Website',
     'description': 'Help with e-services.',
     #'sequence': '1',
     'author': 'Vertel AB',
