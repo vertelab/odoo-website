d1fd7742a81fffc26cffdc8f893a161e8652da64
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-12 11:31:18 +0200

    Update __manifest__.py

--- web_dms_media/__manifest__.py
+++ web_dms_media/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'User can see dms.file on the website media bank.',
-    'category': 'Productivity',
+    'category': 'Website',
     'description': 'User can see dms.file on the website media bank.',
     #'sequence': '1',
     'author': 'Vertel AB',
