125c5c0fb7ca1600f964aa29729153176078e936
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-12 13:34:13 +0200

    Update __manifest__.py

--- base_oldid/__manifest__.py
+++ base_oldid/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Show previous ID.',
-    'category': 'Technical',
+    'category': 'Website',
     'description': 'Show old ID.',
     #'sequence': '1',
     'author': 'Vertel AB',
