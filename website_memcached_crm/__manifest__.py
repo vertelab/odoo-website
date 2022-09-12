87580890c60da658c593fd7730db0362a73fe4d9
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-12 13:33:17 +0200

    Update __manifest__.py

--- website_memcached_crm/__manifest__.py
+++ website_memcached_crm/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Website acceleration for crm using memcached.',
-    'category': 'Technical',
+    'category': 'Website',
     'description': """
 Add mechanisms to cache rendered crm pages.
 This module depends on website_memcached.
