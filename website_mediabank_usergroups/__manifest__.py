38f4b527ba3b09d477d1724eb4c2f08d3dd84541
Jakob Krabbe <jakob.krabbe@vertel.se>
2023-05-09 11:50:15 +0000

    adding minor spelling error

--- website_mediabank_usergroups/__manifest__.py
+++ website_mediabank_usergroups/__manifest__.py
@@ -24,11 +24,10 @@
     'name': 'Website: MediaBank User Groups',
     'version': '14.0.1.2',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    "summary": "Only the right group will be able to use the website media.",
-    "description": """
+    'summary': 'Only the right group will be able to use the website media.',
+    'description': """
     User with the right group will use the website media.
     """,
-    'description': 'Help with e-services.',
     #'sequence': '1',
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/odoo-website/website_mediabank_usergroups',
@@ -38,7 +37,7 @@
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-website',
     'depends': ['website', 'web_editor'],
-    "data": [
+    'data': [
         'views/assets.xml',
     ],
     'qweb': [
