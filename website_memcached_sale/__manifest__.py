4643d1be10b001c4b12d0655d5fa3bd1f2a2f778
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-05-09 11:49:27 +0200

    Update __manifest__.py
    minor spelling errors

--- website_memcached_sale/__manifest__.py
+++ website_memcached_sale/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website: Website MemCached Sale',
+    'name': 'Website: MemCached Sale',
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Website acceleration for Sale using MemCached.',
