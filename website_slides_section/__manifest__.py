7c68e602f1c4458b35ddb85bc3ffde4807414938
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-05-09 11:54:43 +0200

    Update __manifest__.py
    minor spelling errors

--- website_slides_section/__manifest__.py
+++ website_slides_section/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website: Website Slide Section',
+    'name': 'Website: Slide Section',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adding inverse to category_id / section',
