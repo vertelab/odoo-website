b27127655bf40247130721e7fdcb1ca973f44fb5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-05-09 09:58:59 +0200

    Update __manifest__.py
    minor spelling issues

--- website_varnish/__manifest__.py
+++ website_varnish/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website: Website Varnish',
+    'name': 'Website: Varnish',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Website acceleration using varnish',
