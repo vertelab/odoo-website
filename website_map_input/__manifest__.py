0e2cf82ff275803289b91175ed876cc0d54d26fc
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-05-09 12:01:01 +0200

    Update __manifest__.py
    minor spelling errors

--- website_map_input/__manifest__.py
+++ website_map_input/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website: Website Map Input',
+    'name': 'Website: Map Input',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Get latitude and longitude.',
