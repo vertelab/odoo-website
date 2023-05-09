215cb5e0b14e4ae4b36a513bcef58e8f26e985f8
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-05-09 11:56:11 +0200

    Update __manifest__.py
    minor spelling errors

--- website_enable_cors/__manifest__.py
+++ website_enable_cors/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Website: Website Enable CORS',
+    'name': 'Website: Enable CORS',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Enable CORS on controllers.',
